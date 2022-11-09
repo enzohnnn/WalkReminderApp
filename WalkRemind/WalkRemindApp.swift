b3ac5d83d50877aa1bd9311fc24ca7d13bde1f44
enzohnnn <e3han@ucsd.edu>
2022-11-08 20:31:36 -0800

    Initial Commit

---
+++ WalkRemind/WalkRemindApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalkRemindApp.swift
+//  WalkRemind
+//
+//  Created by Enzo Han on 11/8/22.
+//
+
+import SwiftUI
+
+@main
+struct WalkRemindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
